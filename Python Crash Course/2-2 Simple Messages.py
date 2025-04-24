f23dcf3f2ab750ff7b7dcc892bde0b7651948d55
Damani-Holland <damanirayholland@gmail.com>
2025-04-24 12:43:08 -0700

    python crash course chapter 2-1, 2-2 challenge

---
+++ Python Crash Course/2-2 Simple Messages.py
@@ -0,0 +1,19 @@
+###############################################################################
+#   Damani Holland
+#   4/24/2025
+#   CS Python
+###############################################################################
+
+'''
+Store a message in a variable, and print that message.
+Then change the value of your variable to a new message, and print the new
+message.
+'''
+
+message = "Hello user. This is a Python program."
+
+print(message)
+
+message = "Always document any work you do for your studies."
+
+print(message)
