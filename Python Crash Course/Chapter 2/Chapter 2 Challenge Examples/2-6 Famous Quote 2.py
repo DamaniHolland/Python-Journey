fb124cb8cfba23a610ce81ffb472355f82f29b95
Damani-Holland <damanirayholland@gmail.com>
2025-04-24 15:45:40 -0700

    finished strings exercises in chapter 2

---
+++ Python Crash Course/Chapter 2/Chapter 2 Challenge Examples/2-6 Famous Quote 2.py
@@ -0,0 +1,22 @@
+###############################################################################
+#   Damani Holland
+#   4/24/2025
+#   CS Python
+###############################################################################
+
+
+# 2-6 Famous Quote 2
+
+'''
+Repeat Exercise 2-5, but this time store the famous person’s
+name in a variable called famous_person. Then compose your message
+and store it in a new variable called message. Print your message.
+'''
+
+name = 'francis bacon'
+
+quote = '"Knowledge is power."'
+
+message = name.title() + ' once said, ' + quote
+
+print(message)
