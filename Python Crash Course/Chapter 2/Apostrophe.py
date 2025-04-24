0467329ff76774611b2aa2cdc83ac591e459092b
Damani-Holland <damanirayholland@gmail.com>
2025-04-24 14:36:42 -0700

    2-3 challenge Exercise

---
+++ Python Crash Course/Chapter 2/Apostrophe.py
@@ -0,0 +1,16 @@
+###############################################################################
+#   Damani Holland
+#   4/24/2025
+#   CS Python
+###############################################################################
+
+
+# Avoiding Syntax Errors with Strings
+
+message = "One of Python's strenghts is its diverse community."
+
+'''
+The apostrophe appear inside the double quotes, so the python interpreter has 
+no trouble reaading tyhe string correctly
+'''
+print(message)
--- Python Crash Course/Chapter 2/name.py
+++ Python Crash Course/Chapter 2/name.py
@@ -40,3 +40,34 @@ in a variable
 message = "Hello, " + full_name.title() + "!"
 
 print(message)
+
+# Adding Whitespace to Strings with Tabs or Newlines
+
+# '\t' to add tab to your text
+
+print("\tPython")
+
+# '\n' To add a newline in a string
+
+print("Languages:\nPython\nC\nJavascript")
+
+'''
+You can also combine tabs and newlines in a single string. The string (\)n(\)t
+tells python to move to a new line, and start the next line with a tab.
+'''
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+# Stripping Whitspcae
+
+favorite_language = "python "
+punctuation = '!'
+print('With Whitespace: ' + favorite_language + punctuation)
+print('Without Whitespace: ' + favorite_language.rstrip() + punctuation)
+
+
+favorite_language = ' python '
+print('original:' + favorite_language + 'is my favorite language')
+print('.rstrip() method:' + favorite_language.rstrip() + 'is my favorite language')
+print('.lstrip() method:' + favorite_language.lstrip() + 'is my favorite language')
+print('.strp() method:' + favorite_language.strip() + 'is my favorite language')
