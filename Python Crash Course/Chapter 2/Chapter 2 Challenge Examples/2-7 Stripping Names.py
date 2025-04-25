5c8b5bad91cb8a2a2dd501778139a1ca262ead0f
Damani-Holland <damanirayholland@gmail.com>
2025-04-24 21:46:32 -0700

    finished 2-9 Favorite Numbers

--- Python Crash Course/Chapter 2/Chapter 2 Challenge Examples/2-7 Stripping Names.py
+++ Python Crash Course/Chapter 2/Chapter 2 Challenge Examples/2-7 Stripping Names.py
@@ -4,6 +4,8 @@
 #   CS Python
 ###############################################################################
 
+# 2-7 Stripping Names
+
 '''
 Store a person’s name, and include some whitespace
 characters at the beginning and end of the name. Make sure you use each
--- Python Crash Course/Chapter 2/Chapter 2 Challenge Examples/2-8 Number Eight.py
+++ Python Crash Course/Chapter 2/Chapter 2 Challenge Examples/2-8 Number Eight.py
@@ -4,6 +4,7 @@
 #   CS Python
 ###############################################################################
 
+# 2-8 Number Eight
 '''
 Write addition, subtraction, multiplication, and division
 operations that each result in the number 8. Be sure to enclose your operations
