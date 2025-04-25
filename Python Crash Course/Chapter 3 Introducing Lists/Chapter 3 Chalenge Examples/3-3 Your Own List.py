f2e521b22aa387cfcda706538572ca3d3d40595a
Damani-Holland <damanirayholland@gmail.com>
2025-04-24 22:58:24 -0700

    finished 3-3 exercise

---
+++ Python Crash Course/Chapter 3 Introducing Lists/Chapter 3 Chalenge Examples/3-3 Your Own List.py
@@ -0,0 +1,21 @@
+###############################################################################
+#   Damani Holland
+#   4/24/2025
+#   CS Python
+###############################################################################
+
+# 3-3 Your Own List
+
+'''
+Think of your favorite mode of transportation, such as a 
+motorcycle or a car, and make a list that stores several examples . Use your list 
+to print a series of statements about these items, such as “I would like to own a 
+Honda motorcycle .”
+'''
+
+favorite_vehicles = ['car', 'bike', 'skateboard', 'bus']
+
+print("The process of me getting a car started with learning to ride a " + favorite_vehicles[2])
+print("the next progressive step was to learn how to ride a " + favorite_vehicles[1])
+print("The distances i had to travel kept getting larger so I had to learn the " + favorite_vehicles[-1] + " routes.")
+print("When I saw a homeless man mastrubate and touch every handrail on the way out, I decided right then and there that i would need to learn how to drive a " + favorite_vehicles[0])
