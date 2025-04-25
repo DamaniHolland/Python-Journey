f1e8ff4769b0b9491f50447c330590941244202d
Damani-Holland <damanirayholland@gmail.com>
2025-04-24 21:27:25 -0700

    added the pdf's for studies

---
+++ Python Crash Course/Chapter 2/Chapter 2 Challenge Examples/2-8 Number Eight.py
@@ -0,0 +1,13 @@
+###############################################################################
+#   Damani Holland
+#   4/24/2025
+#   CS Python
+###############################################################################
+
+'''
+Write addition, subtraction, multiplication, and division
+operations that each result in the number 8. Be sure to enclose your operations
+in print statements to see the results. You should create four lines that look
+like this:
+'''
+
