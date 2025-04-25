725f5aa1336e9576922a48cef9214b3ad5520236
Damani-Holland <damanirayholland@gmail.com>
2025-04-24 22:31:02 -0700

    finished 3-1 exercise

---
+++ Python Crash Course/Chapter 3 Introducing Lists/bicycles.py
@@ -0,0 +1,28 @@
+###############################################################################
+#   Damani Holland
+#   4/24/2025
+#   CS Python
+###############################################################################
+
+# Introducing Lists
+
+'''
+A lists is a collection of items in a particular order. The list can include 
+letters of the alphabet and/or digits 0-9
+'''
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# Accessing Elements in a list
+
+'''
+To access an item in a list:
+    - name_of_list[index_of_item]
+
+Index Positions start at 0 not 1
+To access last last element in a list simply use [-1] for the index position
+You can acces individual values from a list just as you would any other variable.
+'''
+
+print('The first index in the list is: ' + bicycles[0].title())
