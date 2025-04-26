50296ed13af34c10eb1471aa6b7ab81972bdedff
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-04-25 22:16:21 -0700

    Adding notes on modifying lists

--- Python Crash Course/Chapter 3 Introducing Lists/motorcycles.py
+++ Python Crash Course/Chapter 3 Introducing Lists/motorcycles.py
@@ -19,12 +19,64 @@ print('original list:' , motorcycles)
 #   motorcycles[0] = 'ducati'
 #   print('modified list:' , motorcycles)
 
+
+# Adding Elements to a list  
+
 '''
-Adding Elements to a list
+Appending Elements to the End of a list
 
 Syntax: 
-    - list_name.append('new_value')   
+    - list_name.append(new_value) 
 '''
 
 motorcycles.append('ducati')
 print('modified list:', motorcycles)
+
+'''
+Inserting Elements into a List will shift existing values in list instead of 
+replacing
+
+Syntax:
+    - list_name.insert(new_value)
+'''
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')
+print('Inserting into list:', motorcycles)
+
+
+# Removing Elements from a List
+
+'''
+Removing an Item Using the del Statement
+
+Syntax:
+    - del list_name[index_of_item]
+'''
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print('original list:', motorcycles)
+
+del motorcycles[0]
+print('deleted item using index:', motorcycles)
+
+'''
+Removing an Item Using the .pop() method
+
+Syntax:
+    - list_name.pop()
+    
+removes the last item from the list
+is possible to assign to a variable
+
+Syntax:
+    - var_name = list_name.pop()
+'''
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print('original list:', motorcycles)
+
+popped_motorcycles = motorcycles.pop()
+print('popped list:', motorcycles)
+print('Value popped:', popped_motorcycles)
+
