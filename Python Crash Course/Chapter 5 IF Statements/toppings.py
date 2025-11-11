f8041cf6af93b68e63aaa3ced6969c28710c8341
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-11 06:44:15 -0800

    Inequality Check

--- Python Crash Course/Chapter 5 IF Statements/toppings.py
+++ Python Crash Course/Chapter 5 IF Statements/toppings.py
@@ -9,4 +9,16 @@
 '''
 Checking for Inequality
 
+Syntax: 
+
+    - if variable != element
+          print(element)
+          
+- The (!=) helps to check for inequality where if the if statement is fufilled
+  then it runs the next indented line of code underneath.
 '''
+
+requested_topping = 'mushroom'
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
