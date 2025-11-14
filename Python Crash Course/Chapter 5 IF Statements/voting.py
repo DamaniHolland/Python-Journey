0e343a71b95488215a57298158ba427b43fcbd68
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-14 07:01:43 -0800

    if-else statement

---
+++ Python Crash Course/Chapter 5 IF Statements/voting.py
@@ -0,0 +1,30 @@
+###############################################################################
+#   Damani Holland
+#   11/14/2025
+#   CS Python
+###############################################################################
+# if-else Statements
+
+'''
+if-else Statements
+
+Syntax:
+
+    - if conditional statement:
+          execute code
+      else:
+          execute alternate code
+'''
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+
+age = 17
+if age >= 18:
+    print("\nYou are old enough to vote!")
+    ("Have you registered to vote yet?")
+else:
+    print("\nSorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+    
