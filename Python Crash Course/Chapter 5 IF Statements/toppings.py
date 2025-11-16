a9a99be13386ef3de677428fcf15c76e7015f522
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-16 07:01:08 -0800

    If Statements with Lists

--- Python Crash Course/Chapter 5 IF Statements/toppings.py
+++ Python Crash Course/Chapter 5 IF Statements/toppings.py
@@ -46,3 +46,24 @@ if 'extra cheese' in requested_toppings:
     print("Adding extra cheese")
     
 print("\nFinished making your pizza!")
+
+# Using if Statements with Lists
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print("Adding " + requested_topping + ".")
+
+print("\nFinished making your pizza!")
+
+requested_toppings = []
+
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print("Adding " + requested_topping + ".")
+    print("\nFinished making your pizza!")
+else:
+    print("Are you sure you want a plain pizza?")
