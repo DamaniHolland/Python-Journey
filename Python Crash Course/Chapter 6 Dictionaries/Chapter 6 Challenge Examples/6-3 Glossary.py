c0c66898a14698a1ce139277a30276cda1db2366
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-18 21:31:46 -0800

    6-3 Starting glossary

---
+++ Python Crash Course/Chapter 6 Dictionaries/Chapter 6 Challenge Examples/6-3 Glossary.py
@@ -0,0 +1,33 @@
+###############################################################################
+#   Damani Holland
+#   11/18/2025
+#   CS Python
+###############################################################################
+
+
+'''
+
+6-3. Glossary: A Python dictionary can be used to model an actual dictionary . 
+However, to avoid confusion, let’s call it a glossary .
+
+•  Think of five programming words you’ve learned about in the previous 
+chapters . Use these words as the keys in your glossary, and store their 
+meanings as values .
+
+•  Print each word and its meaning as neatly formatted output . You might 
+print the word followed by a colon and then its meaning, or print the word 
+on one line and then print its meaning indented on a second line . Use the 
+newline character (\n) to insert a blank line between each word-meaning 
+pair in your output
+
+'''
+
+glossary = {}
+
+glossary['lists'] = "A lists is a collection of items in a particular order. The list can include letters of the alphabet and/or digits 0-9."
+                    
+print(glossary['lists'])
+
+
+
+
