0e48097961691e6b53e7d806c43d6bfef3a248af
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-18 22:28:34 -0800

    Looping Through a Dictionary

---
+++ Python Crash Course/Chapter 6 Dictionaries/user.py
@@ -0,0 +1,32 @@
+###############################################################################
+#   Damani Holland
+#   11/19/2025
+#   CS Python
+###############################################################################
+
+# Looping Through a Dictionary
+
+'''
+
+Looping Through All Key-Value Pairs
+
+'''
+    # A dictionary that contains one person's username, first name, 
+    # and last name.
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+    # Created a for loop by using 'for',
+    # Created two variables to hold key value pairs 'username: efermi'
+    # named 'key' and ' value.
+    # called dictionary variable name 'user_0' 
+    # Used .items() method, which returns a list of key-value pairs.
+    # Assinged for loop to every item in user_0 using 'in' method
+
+for key, value in user_0.items():
+    print("\nKey: " + key)
+    print("Value: " + value)
+    
