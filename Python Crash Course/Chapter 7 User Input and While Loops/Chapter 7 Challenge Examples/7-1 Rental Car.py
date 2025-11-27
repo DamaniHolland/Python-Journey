853fedf69bb1e2d7a950244277b61cfd1db70742
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-26 16:27:29 -0800

    7-1 completed

---
+++ Python Crash Course/Chapter 7 User Input and While Loops/Chapter 7 Challenge Examples/7-1 Rental Car.py
@@ -0,0 +1,14 @@
+###############################################################################
+#   Damani Holland
+#   11/26/2025
+#   CS Python
+###############################################################################
+
+'''
+7-1. Rental Car: Write a program that asks the user what kind of rental car they 
+would like . Print a message about that car, such as “Let me see if I can find you 
+a Subaru .”
+'''
+
+rental_car = input("What brand of car would you like to rent? ")
+print("Ah yes, the " + rental_car.title() + "." + " What a wonderful choice.")
