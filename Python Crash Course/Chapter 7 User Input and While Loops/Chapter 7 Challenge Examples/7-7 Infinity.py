ea7580140d7907ce15236878a3539e38f7fbf25f
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-29 11:36:50 -0800

    7-7 completed

---
+++ Python Crash Course/Chapter 7 User Input and While Loops/Chapter 7 Challenge Examples/7-7 Infinity.py
@@ -0,0 +1,15 @@
+###############################################################################
+#   Damani Holland
+#   11/29/2025
+#   CS Python
+###############################################################################
+
+'''
+7-7. Infinity: Write a loop that never ends, and run it . (To end the loop, press 
+ctrl-C or close the window displaying the output .)
+'''
+x = 0
+while x != '':
+    if x != '':
+        x +=1
+        print(x)
