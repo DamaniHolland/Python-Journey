4b642d94e082852d7d01dd2979bd44917f4fa981
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-11-30 09:58:20 -0800

    moving values from one list to another

---
+++ Python Crash Course/Chapter 7 User Input and While Loops/confirmed_user.py
@@ -0,0 +1,34 @@
+###############################################################################
+#   Damani Holland
+#   11/29/2025
+#   CS Python
+###############################################################################
+
+# Moving Items from One List and Dictionaries
+
+    # Moving Items from One List to Another
+
+    # a list of unconfirmed users in a list assigned to 'unconfirmed_users'
+    # assign empty list to 'confirmed_users'
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+
+    # while using values in unconfirmed_users
+        # removes current value from list unconfirmed_users[] 
+        # assign to variable current_user
+        # print 'verifying user: current_user
+        # add current_user value to empty list confirmed_users[]
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+
+    # print string statement
+    # for every value in confirmed_users list
+    # print confirmed_user value
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+    
+# Removing All Instances of Specific Vaues from a List
