6c4c732632b336cdf35efc23239e170ad34fa9d9
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2025-12-06 11:52:05 -0800

    functions

---
+++ Python Crash Course/Chapter 8 Functions/greeter.py
@@ -0,0 +1,25 @@
+###############################################################################
+#   Damani Holland
+#   12/06/2025
+#   CS Python
+###############################################################################
+
+# Functions
+
+'''
+Functions:
+
+    - Named blocks of code that are designed to do one specific job.
+      When you want to perform a particular task that you've defined 
+      in a function, you 'call' the name of the function responsible for it. 
+'''
+
+    # Use 'def' create a function assigned to name 'greet_user"
+        # Print string statements
+def greet_user():
+    
+    print("Hello!")
+    
+    # call function by name 'greet_user'
+    
+greet_user()
