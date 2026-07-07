49a1dab5d97ac93983a3e61cff2f205525eeacb3
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2026-07-07 01:47:09 -0700

    9-7 completed

--- Python Crash Course/Chapter 9 Classes/Chapter 9 Challenge Examples/9-7 Admin.py
+++ Python Crash Course/Chapter 9 Classes/Chapter 9 Challenge Examples/9-7 Admin.py
@@ -21,3 +21,51 @@ of the user’s information . Make another method called greet_user() that print
 a personalized greeting to the user .Create several instances representing different 
 users, and call both methods for each user .
 '''
+class User():
+    
+    def __init__(self, first_name, last_name, username, email,):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        
+    def describe_user(self):
+        message = "User Information:"
+        message += "\n\tFirst Name: " + self.first_name.title()
+        message += "\n\tLast Name: " + self.last_name.title()
+        message += "\n\tUsername: " + self.username
+        message += "\n\tEmail: " + self.email
+        
+        print(message)
+    
+    def greet_user(self):
+        message = "Greetings " + self.first_name.title()
+        message += "\nWelcome to the Software!"
+        
+        print(message)
+        
+class Admin(User):
+    
+    def __init__(self, first_name, last_name, username, email):
+        super(Admin, self).__init__(first_name, last_name, username, email)
+        self.privileges = ["can add post", "can delete post", "can ban user"]
+    
+    def describe_privileges(self):
+        print("List of Privileges: ")
+        for privilege in self.privileges:
+            message = "\n\t" + privilege
+            print(message)
+            
+            
+        
+user_One = Admin("damani", "holland", "daho", "damanirayholland@gmail.com")
+user_Two = User('amiri', 'aje', 'amaj', 'amiri.aje@gmail.com')
+
+user_One.describe_user()
+user_One.greet_user()
+user_One.describe_privileges()
+
+
+
+user_Two.describe_user()
+user_Two.greet_user()
