3948dd0dbb67dc758ba4ee1a415eb20f8f8685fb
damani-holland <77711897+DamaniHolland@users.noreply.github.com>
2026-07-08 00:05:03 -0700

    importing and entire module

--- Python Crash Course/Chapter 9 Classes/import_car.py
+++ Python Crash Course/Chapter 9 Classes/import_car.py
@@ -13,3 +13,23 @@ print(my_new_car.get_descriptive_name())
 
 my_new_car.odometer_reading =23
 my_new_car.read_odometer()
+
+# Importing Multiple Classes from aModule
+
+from car import Car, ElectricCar
+
+my_beetle = Car('volkswagen', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = ElectricCar('tesla', 'roadster', 2016)
+print(my_tesla.get_descriptive_name())
+
+# Importing an Entire Module
+
+import car
+
+my_beetle = car.Car('volkswagen', 'beetle', 2016)
+print(my_beetle.get_descriptive_name())
+
+my_tesla = car.ElectricCar('tesla', 'roadster', 2016)
+print(my_tesla.get_descriptive_name())
