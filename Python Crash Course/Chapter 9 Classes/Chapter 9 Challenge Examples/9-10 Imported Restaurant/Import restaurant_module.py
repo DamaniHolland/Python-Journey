38118189d1229eab934a998750ebaef3db3083ac
Damani-Holland <damanirayholland@gmail.com>
2026-07-08 16:10:53 -0700

    9-10 completed

---
+++ Python Crash Course/Chapter 9 Classes/Chapter 9 Challenge Examples/9-10 Imported Restaurant/Import restaurant_module.py
@@ -0,0 +1,13 @@
+###############################################################################
+#   Damani Holland
+#   7/8/2026
+#   CS Python
+###############################################################################
+
+from Restaurant_Module import Restaurant
+
+
+restaurant_One = Restaurant('dominos', 'italian')
+
+restaurant_One.open_restaurant()
+restaurant_One.describe_restaurant()
