2ca8579c31c30be5ba93d06fdcf8fdb0de13a636
Damani-Holland <damanirayholland@gmail.com>
2026-07-10 17:46:18 -0700

    Refactor file reading logic to use a variable for file name and improve output formatting

--- Python Crash Course/Chapter 10 Files and Exceptions/file_reader.py
+++ Python Crash Course/Chapter 10 Files and Exceptions/file_reader.py
@@ -3,7 +3,21 @@
 #   7/10/2026
 #   CS Python
 ###############################################################################
-
-with open('pi_digits.txt') as file_object:
+# Reading an Entire File
+file_name = 'Python Crash Course\Chapter 10 Files and Exceptions\pi_digits.txt'
+""" We store the name of the file we're reading from in the variable 'file_name' """
+with open(file_name) as file_object:
+    """ Use 'with' syntax to let Python open and close the file properly."""
     contents = file_object.read()
-    print(contents)
+    print(contents.rstrip())
+
+
+# Reading Line by Line
+with open(file_name) as file_object:
+    for line in file_object:
+        """Use 'for to work through each line in the file by looping over the 
+            file object.
+        """
+        print(line.rstrip()) # Using '.rstrip()'on each line in the print 
+                            # statement eliminates these extra blank lines
+                            
